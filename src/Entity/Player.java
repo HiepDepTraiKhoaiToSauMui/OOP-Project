bff3c39bc4859083681027b123287583624c7cb5
KhoaVo128 <buttskicker128@gmail.com>
2022-04-28 20:26:59 +0700

    aaaaaaa

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -32,14 +32,14 @@ public class Player extends Entity{
     public void getPlayerImage() {
 
         try {
-            up1 = ImageIO.read(getClass().getResourceAsStream("/player/bear_up_1.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("/player/bear_up_2.png"));
-            down1 = ImageIO.read(getClass().getResourceAsStream("/player/bear_down_1.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("/player/bear_down_2.png"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("/player/bear_right_1.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("/player/bear_right_2.png"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("/player/bear_left_1.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("/player/bear_left_2.png"));
+            up1 = ImageIO.read(getClass().getResourceAsStream("/detective/detective_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/detective/detective_up_1.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/detective/detective_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/detective/detective_down_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/detective/detective_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/detective/detective_right_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/detective/detective_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/detective/detective_2.png"));
         } catch(IOException e) {
             e.printStackTrace();
         }
