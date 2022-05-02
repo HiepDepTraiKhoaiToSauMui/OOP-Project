f20535da90da0401a2e45fcd961817e5f528785b
KhoaVo128 <buttskicker128@gmail.com>
2022-05-02 10:31:14 +0700

    Fixxed

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -38,8 +38,8 @@ public class Player extends Entity{
             down2 = ImageIO.read(getClass().getResourceAsStream("/detective/detective_down_2.png"));
             right1 = ImageIO.read(getClass().getResourceAsStream("/detective/detective_right_1.png"));
             right2 = ImageIO.read(getClass().getResourceAsStream("/detective/detective_right_2.png"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("/detective/detective_1.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("/detective/detective_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/detective/detective_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/detective/detective_left_2.png"));
         } catch(IOException e) {
             e.printStackTrace();
         }
